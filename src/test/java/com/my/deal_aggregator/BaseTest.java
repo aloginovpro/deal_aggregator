b503db3508358d9b9c52bd637cc0004336753649
aloginovpro <aloginovpro@gmail.com>
2025-01-02 22:50:06 +0300

    постановка задачи

--- src/test/java/com/my/deal_aggregator/BaseTest.java
+++ src/test/java/com/my/deal_aggregator/BaseTest.java
@@ -3,8 +3,10 @@ package com.my.deal_aggregator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 abstract class BaseTest {
     
     @Autowired
